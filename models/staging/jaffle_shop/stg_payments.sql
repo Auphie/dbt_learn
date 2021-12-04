c70b2d5570144c5089e961a76a1fda42949dfff3
Auphie Chen <auphie@gmail.com>
2021-12-03 22:49:56 -0800

    test

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -3,6 +3,6 @@ select
     ORDERID	as order_id,
     PAYMENTMETHOD as payment_method,
     STATUS as pay_status,
-    AMOUNT/100 as pay_amount,
+    {{- cents_to_dollars('AMOUNT') }} as pay_amount,
     CREATED as created_at
 from {{ source('bq_stripe','payment') }}
