4d203f0e216a3bf069f70ffa733df5b1b9e91d2c
Auphie <34787059+Auphie@users.noreply.github.com>
2025-03-22 15:37:41 -0700

    change code in covered area

--- models/example/sample_s1.sql
+++ models/example/sample_s1.sql
@@ -4,7 +4,6 @@
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
 
-    Try changing "table" to "view" below
 */
 
 with source_data as (
